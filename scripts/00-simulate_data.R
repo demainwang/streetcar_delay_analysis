1fe88c572dd85791dc9e8dd780c655cba9ce861e
demainwang <stevenn.wang@mail.utoronto.ca>
2024-09-23 20:36:14 -0400

    Simulate the interest variables from streetcar delay data

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,19 +1,39 @@
 #### Preamble ####
-# Purpose: Simulates... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Simulates streetcar's delay data in Toronto
+# Author: YITONG WANG
+# Date: 29 September 2024
+# Contact: stevenn.wang@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: None
+# Any other information needed? None
 
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
 
 #### Simulate data ####
-# [...ADD CODE HERE...]
+set.seed(333)
 
+# define possible location name
+Location <- c("QUEENS QUAY AND HARBOU", "SPADINA AND QUEENS QUA", 
+             "KIING AND BATHURST", "EGLINTON GARAGE", "COLLEGE AND OSSINGTON")
 
+# define direction of streetcar
+Bound <- c("E", "S", "W", "N")
 
+# define start date and end date
+start_d <- as.Date("2023-03-01")
+end_d <- as.Date("2023-09-01")
+
+# set number of observations
+n <- 500
+
+# simulate the data
+simulated_data <- tibble(
+  date = s_Date <- sample(seq(start_d, end_d, by = "day"), size = n, replace = TRUE),
+  location = s_loca <- sample(Location, size = n, replace = TRUE),
+  min_delay = s_min_delay <- sample(1:59, size = n, replace = TRUE),
+  bound = s_bound <- sample(Bound, size = n, replace = TRUE))
+
+# storage simulated data
+write.csv(simulated_data, file = "streetcar_delay_analysis/data/raw_data/simulated_data.csv")
