f1d457eeda49f42c571131d937b239ab9ef9c78d
demainwang <stevenn.wang@mail.utoronto.ca>
2024-09-24 14:18:54 -0400

    Remove extreme values or N/A values from the raw data, select the interest column

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -1,44 +1,24 @@
 #### Preamble ####
 # Purpose: Cleans the raw plane data recorded by two observers..... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 6 April 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Author: YITONG WANG
+# Date: 29 September 2024
+# Contact: stevenn.wang@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: None
+# Any other information needed? None
 
 #### Workspace setup ####
 library(tidyverse)
 
 #### Clean data ####
-raw_data <- read_csv("inputs/data/plane_data.csv")
+#delete any row contains N/A value or 0 delay time
+raw_data <- read_csv("streetcar_delay_analysis/data/raw_data/2023_streetcar.csv")
+sum(is.na(data))
+data1 <- raw_data %>% mutate(`Min Delay` = as.numeric(trimws(`Min Delay`)))
+data1 <- na.omit(data1)
+data3 <- data1 %>% filter(`Min Delay` > 0 & `Min Delay` < 60)
+cleaned_data <- data3 %>% select(Day, Location, `Min Delay`, Bound)
 
-cleaned_data <-
-  raw_data |>
-  janitor::clean_names() |>
-  select(wing_width_mm, wing_length_mm, flying_time_sec_first_timer) |>
-  filter(wing_width_mm != "caw") |>
-  mutate(
-    flying_time_sec_first_timer = if_else(flying_time_sec_first_timer == "1,35",
-                                   "1.35",
-                                   flying_time_sec_first_timer)
-  ) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "490",
-                                 "49",
-                                 wing_width_mm)) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "6",
-                                 "60",
-                                 wing_width_mm)) |>
-  mutate(
-    wing_width_mm = as.numeric(wing_width_mm),
-    wing_length_mm = as.numeric(wing_length_mm),
-    flying_time_sec_first_timer = as.numeric(flying_time_sec_first_timer)
-  ) |>
-  rename(flying_time = flying_time_sec_first_timer,
-         width = wing_width_mm,
-         length = wing_length_mm
-         ) |> 
-  tidyr::drop_na()
 
 #### Save data ####
 write_csv(cleaned_data, "outputs/data/analysis_data.csv")
