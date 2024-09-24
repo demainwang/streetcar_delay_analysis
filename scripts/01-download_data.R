2f4b387d016f4e0e217422bac10fb0e1f9d88ead
demainwang <stevenn.wang@mail.utoronto.ca>
2024-09-23 21:17:06 -0400

    download 2023 streetcar delay data from opendatatoronto

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,8 +1,8 @@
 #### Preamble ####
 # Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Author: YITONG WANG
+# Date: 29 September 2024
+# Contact: stevenn.wang@mail.utoronto.ca
 # License: MIT
 # Pre-requisites: [...UPDATE THIS...]
 # Any other information needed? [...UPDATE THIS...]
@@ -11,16 +11,25 @@
 #### Workspace setup ####
 library(opendatatoronto)
 library(tidyverse)
-# [...UPDATE THIS...]
+library(dplyr)
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
+# get package
+package <- show_package("b68cb71b-44a7-4394-97e2-5d2f41462a5d")
+package
 
+# get all resources for this package
+resources <- list_package_resources("b68cb71b-44a7-4394-97e2-5d2f41462a5d")
+
+# identify datastore resources; by default, Toronto Open Data sets datastore resource format to CSV for non-geospatial and GeoJSON for geospatial resources
+datastore_resources <- filter(resources, tolower(format) == 'xlsx')
+
+# load the first datastore resource as a sample
+data <- filter(datastore_resources, row_number()==12) %>% get_resource()
+data
 
 
 #### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+write_csv(data, "streetcar_delay_analysis/data/raw_data/2023_streetcar.csv") 
 
          
